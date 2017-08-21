4ec84af960e4b62102d4f93222c7af880d147a60
Wyrembak <mwyrembak@shg-mc-006045.nexus.specialistholidays.com>
2017-08-21 11:44:43 +0100

    certificates

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cloud-aws
 Scripts to be used with Amazon Web Services
+
+aws-certificates.sh
+-------------------
+* List certificates by expiration date
+* Find which ELB has attached selected certificate
+
+Helpful commands to be run manually
+* List certificates by expiration date
+`aws iam list-server-certificates --output text --query 'ServerCertificateMetadataList[*].[Expiration,ServerCertificateName]' | sort`
+* Show certificate details
+`aws iam get-server-certificate --server-certificate-name [certificate_name] --output text --query 'ServerCertificate.CertificateBody' | openssl x509 -text | less`
---
+++ aws-certificates.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# List certificates by expiration date
+# Find which ELB has attached selected certificate
+# --------------------------------------------------------
+# Created by  : Marcin Wyrembak
+# --------------------------------------------------------
+
+confirmSelection() {
+    echo "Continue?"
+    read -p "[y/N]" CONTINUE
+    case ${CONTINUE} in
+        Y|y)
+            echo "Processing..."
+        ;;
+        *)
+            echo "Canceled"
+            exit 1
+        ;;
+    esac
+}
+showCerts() {
+    aws elb describe-load-balancers --region ${awsregion} --output text  --query "LoadBalancerDescriptions[*].{ID:LoadBalancerName,LIS:ListenerDescriptions[*].Listener}" | grep -B1 "${certName}"
+}
+
+checkAndPrepareBatch() {
+    echo "Getting list of certs..."
+    certs=$(aws iam list-server-certificates --output text --query 'ServerCertificateMetadataList[*].[Expiration,ServerCertificateName]' | sort | sed 's/Z[[:space:]]*/__/g; s/T/_/g')
+    echo "Choose cert to Check which ELB has it attached"
+    select option in "Quit" ${certs}; do
+        if [ "${option}" = "Quit" ]; then
+            exit 0
+        elif [ -n "${option}" ]; then
+            echo "You selected: ${option}"
+            certName=$(echo ${option} | awk -F__ '{print $2'})
+            echo "Select region"
+            awsregions=$(aws ec2 describe-regions --query 'Regions[].{Name:RegionName}' --output text)
+            select region in "Quit" "All" ${awsregions}; do
+                if [[ "${region}" == "Quit" ]]; then
+                    exit 0
+                elif [[ "${region}" == "All" ]]; then
+                    echo "You selected: ${region}"
+                    while read -r awsregion; do
+                        echo "------------------------------------------------------------------------------------------------"
+                        echo "==--${awsregion} --=="
+                        echo "------------------------------------------------------------------------------------------------"
+                        showCerts
+                    done <<< "${awsregions}"
+                elif [ -n "${region}" ]; then
+                    awsregion=${region}
+                    echo "You selected: ${awsregion}"
+                    showCerts
+                fi
+            done
+        else
+            echo "Canceled"
+        exit 1
+        fi
+    done
+}
+
+echo "---------------------------------------"
+echo "Certificates sorted by expiration date"
+echo "---------------------------------------"
+checkAndPrepareBatch
